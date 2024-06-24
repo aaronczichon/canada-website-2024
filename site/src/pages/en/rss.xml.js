cc6d3bfe7e635b5b37dcf93516410102c1ee15d4
Aaron Czichon <mail@aaronczichon.de>
2024-06-24 17:09:53 +0000

    content: making lists and stuff correctly working for translated version

--- site/src/pages/en/rss.xml.js
+++ site/src/pages/en/rss.xml.js
@@ -4,10 +4,10 @@ import rss from '@astrojs/rss';
 export async function GET(_context) {
 	const blog = await getCollection('blog');
 	return rss({
-		title: 'Canada - Working Holiday für 12 Monate - Blog',
+		title: 'Canada - Working Holiday for 1 months - Blog',
 		// `<description>` field in output xml
-		description: 'Für 12 Monate reisend und arbeitend durch Kanada.',
-		site: 'https://canada.aaronczichon.de',
+		description: 'We\' working and travelling 12 months through Canada.',
+		site: 'https://canada.aaronczichon.de/en',
 		items: blog.map((post) => ({
 			title: post.data.title,
 			pubDate: post.data.pubDate,
@@ -15,7 +15,7 @@ export async function GET(_context) {
 			customData: post.data.customData,
 			// Compute RSS link from post `slug`
 			// This example assumes all posts are rendered as `/blog/[slug]` routes
-			link: `/blog/${post.slug}/`,
+			link: `/en/blog/${post.slug}/`,
 		})),
 	});
 }
