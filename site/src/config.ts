90c0ca41f482ed3615f095965e1de3f0251bd932
Aaron Czichon <mail@aaronczichon.de>
2024-06-24 16:20:12 +0000

    feat: translated nav menu

--- site/src/config.ts
+++ site/src/config.ts
@@ -5,3 +5,22 @@ export const GLOBAL_CONFIG = {
   defaultHeight: 450,
   defaultQuality: 80,
 };
+
+export const NAVIGATION_TRANSLATIONS = {
+  en: {
+    blog: "Blog",
+    map: "Map",
+    pengiun: "Find Pengiuns",
+    visitors: "Visitors",
+    about: "About us",
+    imprint: "Imprint",
+  },
+  de: {
+    blog: "Blog",
+    map: "Karte",
+    pengiun: "Find Pengiuns",
+    visitors: "Besucher",
+    about: "Über uns",
+    imprint: "Impressum",
+  },
+};
