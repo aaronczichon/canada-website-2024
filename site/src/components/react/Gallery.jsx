19bb02713ce5b9a42655d3be9e1191d45480746f
Aaron Czichon <mail@aaronczichon.de>
2024-07-04 17:31:29 +0000

    feat: adding gallery component and react support

---
+++ site/src/components/react/Gallery.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+
+export default function GridGallery({ images, lang }) {
+  const [open, setOpen] = React.useState(false);
+  const [btnText, setBtnText] = React.useState("missing.translation");
+
+  React.useEffect(() => {
+    if (lang === "en") {
+      setBtnText("Open Gallery");
+    } else {
+      setBtnText("Photos anzeigen");
+    }
+  }, [lang]);
+
+  return (
+    <div className="gallery-container">
+      {images.length > 0 ? (
+        <>
+          <button type="button" onClick={() => setOpen(true)}>
+            {btnText}
+          </button>
+
+          <Lightbox
+            open={open}
+            close={() => setOpen(false)}
+            slides={images.map((src) => ({ src }))}
+          />
+        </>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+}
