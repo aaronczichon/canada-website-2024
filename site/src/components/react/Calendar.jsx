07b2a54d8f1fcdc9b7cfed0a671a84d237d87136
Aaron Czichon <mail@aaronczichon.de>
2024-07-15 14:53:24 +0000

    feat: dynamic calendar component, updates to visiting page
    refs: #7

--- site/src/components/react/Calendar.jsx
+++ site/src/components/react/Calendar.jsx
@@ -3,8 +3,10 @@ import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 import deLocale from "@fullcalendar/core/locales/de";
 import enLocale from "@fullcalendar/core/locales/en-gb";
 
-export default function Calendar({ locale }) {
-  const lang = locale === "de" || locale === undefined ? deLocale : enLocale;
+export default function Calendar() {
+  const locale = window.navigator.language || window.navigator.userLanguage;
+  const loc = locale.split("-")[0];
+  const lang = loc === "de" ? deLocale : enLocale;
   return (
     <FullCalendar
       plugins={[dayGridPlugin]}
