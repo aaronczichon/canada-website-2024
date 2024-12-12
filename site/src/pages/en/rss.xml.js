81d34d0feaf9911437adee99078ad9c6331fc580
Aaron Czichon <mail@aaronczichon.de>
2024-12-12 23:47:30 +0000

    feat: added full text RSS support for EN

--- site/src/pages/en/rss.xml.js
+++ site/src/pages/en/rss.xml.js
@@ -1,22 +1,32 @@
+import { experimental_AstroContainer as AstroContainer } from 'astro/container';
+import { getContainerRenderer as getMDXRenderer } from '@astrojs/mdx';
+import { getContainerRenderer as getPreactRenderer } from '@astrojs/preact';
+import { loadRenderers } from 'astro:container';
 import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
 
-export async function GET(_context) {
+export async function GET(context) {
+	const renderers = await loadRenderers([getPreactRenderer(), getMDXRenderer()]);
+	const container = await AstroContainer.create({ renderers });
+	container.addClientRenderer({
+		name: '@astrojs/preact',
+		entrypoint: '@astrojs/preact/client.js',
+	});
+
 	let entries = await getCollection('blog');
 	entries = entries.filter((entry) => entry.slug.split('/')[0] === 'en');
+	const feedItems = [];
+	for (const post of entries) {
+		const { Content } = await post.render();
+		const content = await container.renderToString(Content);
+		const link = new URL(`/en/blog/${post.slug}`, context.url.origin).toString();
+		feedItems.push({ ...post.data, link, content });
+	}
 	return rss({
 		title: 'Canada - Working Holiday for 1 months - Blog',
 		// `<description>` field in output xml
 		description: "We' working and travelling 12 months through Canada.",
 		site: 'https://canada.aaronczichon.de/en',
-		items: entries.map((post) => ({
-			title: post.data.title,
-			pubDate: post.data.pubDate,
-			description: post.data.description,
-			customData: post.data.customData,
-			// Compute RSS link from post `slug`
-			// This example assumes all posts are rendered as `/blog/[slug]` routes
-			link: `/en/blog/${post.slug}/`,
-		})),
+		items: feedItems,
 	});
 }
